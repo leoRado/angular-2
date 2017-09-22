1e2348c545e007441f97dc5393efbf1ccf8a77f7
Leonhard Radonic <leonhard.radonic@gmail.com>
2017-09-22 16:27:22 +0200

    deleted js files

--- app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var _14_inputs_outputs_1 = require('./14.inputs.outputs');
-platform_browser_dynamic_1.bootstrap(_14_inputs_outputs_1.AppComponent);
-//# sourceMappingURL=main.js.map
